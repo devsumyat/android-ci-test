53e6ec82ddb12611c5245be26868fafaf1f11c08
su myat <sumyat@sus-MacBook-Pro.local>
2019-06-10 15:40:22 +0630

    test

--- app/src/main/java/me/vponomarenko/androidcircleci/MainActivity.kt
+++ app/src/main/java/me/vponomarenko/androidcircleci/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     private val textReverser = TextReverser()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
